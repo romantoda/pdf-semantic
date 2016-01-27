96e2487f280fd6b25734b86b9ddef39f911dc652
jozef <jozef.baranec@gmail.com>
2016-01-27 23:31:38 +0100

    Pdfix update

--- src/java/pdfix/pdfixlib/Pdfix.java
+++ src/java/pdfix/pdfixlib/Pdfix.java
@@ -32,7 +32,7 @@ public class Pdfix extends Object {
       System.err.println("Native code library failed to load." + e);
       System.exit(1);    }
     catch (IOException ex) {
-      Logger.getLogger(pdfix.class.getName()).log(Level.SEVERE, null, ex);
+      Logger.getLogger(Pdfix.class.getName()).log(Level.SEVERE, null, ex);
     }
   }
 
